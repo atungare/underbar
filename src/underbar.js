cf48b0b49161a6f26c882e5cb76bab886f361e92
Ajay Tungare <ajay.tungare@gmail.com>
2014-04-23 15:54:24 -0400

    write _.some()

--- src/underbar.js
+++ src/underbar.js
@@ -219,6 +219,13 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
+
+    var result = _.reduce(collection, function(anyTrue, item) {
+      return (anyTrue || iterator(item));
+    }, false);
+
+    return !!(result);
   };
 
 
